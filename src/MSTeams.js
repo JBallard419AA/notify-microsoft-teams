4de9d13db2d50714b41e3287b7a978bdce75a6fc
Jamey Ballard <Jamey.Ballard@insight.com>
2025-08-06 14:37:59 -0500

    updates

--- src/MSTeams.js
+++ src/MSTeams.js
@@ -15,7 +15,7 @@ const {
       url: placeholder
     },
     commits = [],
-    ref_name,
+    ref,
     head_commit = {
       timestamp: placeholder
     }
@@ -215,7 +215,7 @@ class MSTeams {
       type: 'TextBlock',
       size: 'Medium',
       weight: 'Bolder',
-      text: title !== '' ? title : `${sender.login} ${eventName} initialised workflow"${workflow} on branch ${ref_name}"`,
+      text: title !== '' ? title : `${sender.login} ${eventName} initialised workflow"${workflow} on branch ${ref}"`,
       style: 'heading',
       wrap: true
     };
